5aa6137cd4aeaa4891b50a794c95fa4cca84025d
mohammadazky <mohammadazkykamil@gmail.com>
2025-08-13 20:29:13 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = `I Have Something`.split('')
+const text = `Hai Amelia Puspita Sari Sang Pemilik Bunga Seluruh Dunia`.split('')
 
 // Create container for better responsive layout
 title.style.display = 'flex'
