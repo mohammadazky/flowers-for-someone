fd81a49a5b754254c5f253affea64046fbf9e6c3
mohammadazky <mohammadazkykamil@gmail.com>
2026-02-10 15:41:20 +0700

    Update title text in main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('MENTARI INDAH DAN LUCU').split('')
+    const titles = ('Senyum terus ya , i like it ').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
