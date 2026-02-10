8b559986db97c8d0b182c30d67195c8cc714a5cf
mohammadazky <mohammadazkykamil@gmail.com>
2026-02-10 15:40:26 +0700

    Update greeting text in index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = `Hai Amelia Puspita Sari Sang Pemilik Bunga Seluruh Dunia`.split('')
+const text = `Hai Rhenia Rahmawati , you are very beautiful `.split('')
 
 // Create container for better responsive layout
 title.style.display = 'flex'
